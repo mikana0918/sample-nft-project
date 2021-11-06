1baec9c02f4600c7fbedfc03ebfeb89597ac6d0e
mikana0918 <shogo.kusuhara@gmail.com>
2021-11-06 15:58:38 +0900

    chore: :package: npm install @nomiclabs/hardhat-ethers --save-dev

--- ethereum/contracts/EmotionalShapes.sol
+++ ethereum/contracts/EmotionalShapes.sol
@@ -11,7 +11,7 @@ contract EmotionalShapes is ERC721 {
     constructor() ERC721("EmotionalShapes", "ESS") {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "YOUR_API_URL/api/erc721/";
+        return "https://ba9c-114-184-192-144.ngrok.io/api/erc721/";
     }
 
     function mint(address to)
